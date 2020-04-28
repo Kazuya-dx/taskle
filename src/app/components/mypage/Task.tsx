65dfe90c3e2bdd38a4936d8f042bfbd7a1a902b4
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 22:58:52 +0900

    ビジネスロジックをカスタムフックへ移行

--- src/app/components/mypage/Task.tsx
+++ src/app/components/mypage/Task.tsx
@@ -1,40 +1,15 @@
 import { useState } from "react";
 import TaskList from "./TaskList";
-import createRandomId from "../../modules/create-random-id";
-import getStringFromDate from "../../modules/date_to_string";
 import styles from "./Task.module.scss";
-
-// Redux関連のライブラリ・ファイル
-import { useSelector, useDispatch } from "react-redux";
-import { addUsersTasks } from "../../redux/slices/usersTasksSlice";
-import { RootState } from "../../redux/rootReducer";
-
-// Firebase 初期化(初期化は一度だけ)
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import firebaseConfig from "../../firebaseConfig";
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-interface TaskType {
-  id: string;
-  title: string;
-  text: string;
-  created_at: string;
-  tags: any[];
-}
+import useAddTask from "hooks/useAddTask";
 
 const Task = () => {
-  const user = useSelector((state: RootState) => state.user);
-  const dispatch = useDispatch();
   const [text, setText] = useState("");
   const [title, setTitle] = useState("");
   const [tagstext, setTagstext] = useState("");
   const [isPrivate, setIsPrivate] = useState(false);
   const [toggle, setToggle] = useState(false);
+  const addTask = useAddTask();
 
   return (
     <div>
@@ -96,81 +71,7 @@ const Task = () => {
             <button
               className={styles.button}
               onClick={async () => {
-                let tmpDate = new Date();
-                let now = getStringFromDate(tmpDate);
-                let id = "";
-                const tmpTags: any = [];
-                const tags = tagstext.split(" ");
-                const db = await firebase.firestore();
-
-                // taskをFirestoreに追加
-                await db
-                  .collection("task")
-                  .add({
-                    title: title,
-                    text: text,
-                    is_private: isPrivate,
-                    good: 0,
-                    created_at: now,
-                    uid: user.uid,
-                  })
-                  .then((doc) => {
-                    id = doc.id;
-                  })
-                  .catch((error) => {
-                    console.log(`データの取得に失敗しました (${error})`);
-                  });
-
-                // tagとtag_mapをFirestoreに追加
-                for (let item in tags) {
-                  await db
-                    .collection("tag")
-                    .where("name", "==", tags[item])
-                    .get()
-                    .then(async (snapshot) => {
-                      // 重複なし
-                      if (snapshot.empty) {
-                        let tmp;
-                        await db
-                          .collection("tag")
-                          .add({ name: tags[item] })
-                          .then((doc) => {
-                            tmp = doc.id;
-                          });
-                        await db
-                          .collection("tag_map")
-                          .add({ entry_id: id, tag_id: tmp });
-                        tmpTags.push({ id: tmp, name: tags[item] });
-
-                        // 重複あり
-                      } else {
-                        let tmp;
-                        await snapshot.forEach((doc) => {
-                          tmp = doc.id;
-                        });
-                        await db
-                          .collection("tag_map")
-                          .add({
-                            entry_id: id,
-                            tag_id: tmp,
-                          })
-                          .then(() => {
-                            tmpTags.push({ id: tmp, name: tags[item] });
-                          });
-                      }
-                    });
-                }
-
-                // Redux State 更新
-                const tmpTask: TaskType = {
-                  id: createRandomId(),
-                  title: title,
-                  text: text,
-                  created_at: now,
-                  tags: tmpTags,
-                };
-                dispatch(addUsersTasks(tmpTask));
-
+                await addTask(text, title, tagstext, isPrivate);
                 await setTitle("");
                 await setTagstext("");
                 await setText("");
---
+++ src/app/hooks/useAddTask.tsx
@@ -0,0 +1,107 @@
+// Redux関連のライブラリ・ファイル
+import { useSelector, useDispatch } from "react-redux";
+import { addUsersTasks } from "../redux/slices/usersTasksSlice";
+import { RootState } from "../redux/rootReducer";
+import getStringFromDate from "modules/date_to_string";
+import createRandomId from "modules/create-random-id";
+
+// Firebase 初期化(初期化は一度だけ)
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from "../firebaseConfig";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+interface TaskType {
+  id: string;
+  title: string;
+  text: string;
+  created_at: string;
+  tags: any[];
+}
+
+const useAddTask = () => {
+  const user = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
+
+  const addTask = async (text, title, tagstext, isPrivate) => {
+    let tmpDate = new Date();
+    let now = getStringFromDate(tmpDate);
+    let id = "";
+    const tmpTags: any = [];
+    const tags = tagstext.split(" ");
+    const db = await firebase.firestore();
+    await db
+      .collection("task")
+      .add({
+        title: title,
+        text: text,
+        is_private: isPrivate,
+        good: 0,
+        created_at: now,
+        uid: user.uid,
+      })
+      .then((doc) => {
+        id = doc.id;
+      })
+      .catch((error) => {
+        console.log(`データの取得に失敗しました (${error})`);
+      });
+
+    if (tags[0] != "") {
+      for (let item in tags) {
+        await db
+          .collection("tag")
+          .where("name", "==", tags[item])
+          .get()
+          .then(async (snapshot) => {
+            // 重複なし
+            if (snapshot.empty) {
+              let tmp;
+              await db
+                .collection("tag")
+                .add({ name: tags[item] })
+                .then((doc) => {
+                  tmp = doc.id;
+                });
+              await db.collection("tag_map").add({ entry_id: id, tag_id: tmp });
+              tmpTags.push({ id: tmp, name: tags[item] });
+
+              // 重複あり
+            } else {
+              let tmp;
+              await snapshot.forEach((doc) => {
+                tmp = doc.id;
+              });
+              await db
+                .collection("tag_map")
+                .add({
+                  entry_id: id,
+                  tag_id: tmp,
+                })
+                .then(() => {
+                  tmpTags.push({ id: tmp, name: tags[item] });
+                });
+            }
+          });
+      }
+    }
+
+    // Redux State 更新
+    const tmpTask: TaskType = {
+      id: createRandomId(),
+      title: title,
+      text: text,
+      created_at: now,
+      tags: tmpTags,
+    };
+    dispatch(addUsersTasks(tmpTask));
+  };
+
+  return addTask;
+};
+
+export default useAddTask;
